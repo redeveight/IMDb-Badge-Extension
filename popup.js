d41891d649a4cc11208bc7a7a312ac3570567017
redeveight <redeveight@gmail.com>
2020-11-16 00:34:38 +0300

    add checking url and page status

--- popup.js
+++ popup.js
@@ -1,48 +1,32 @@
+let url = "https://www.imdb.com/badge/";
 var a=0;
 
-function on_apply_click() {
+async function on_apply_click() {
   a++;
   document.getElementById('demo').textContent = a;
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {file: 'inject.js'});
-  });
+  document.getElementById('demo').textContent = "before";
+  open_url(url);
+  
 }
 document.getElementById("btn").onclick = on_apply_click;
 
-
-/*document.getElementById("changeColor").addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementById("changeColor").innerHTML = "YOU CLICKED ME!";
-  console.log("Hello");
-}*/
-
-//let url = "https://www.imdb.com/badge/";
-
-/*chrome.storage.sync.get('color', function(data) {
-  //apply_button.setAttribute('value', data.color);
-});*/
-
-//button.onclick = function(element) {
-  /*chrome.tabs.getSelected(null,function(tab) {
+function open_url(url) {
+  chrome.tabs.getSelected(null, function(tab) {
     if (tab.url != url) {
-      await chrome.tabs.update({
+      chrome.tabs.update({
         url: "https://www.imdb.com/badge/"
       });
+      chrome.tabs.onUpdated.addListener(function (tabId , info) {
+        if (info.status === 'complete') {
+          document.getElementById('demo').textContent = "after";
+          /*chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+              tabs[0].id,
+              {file: 'inject.js'});
+          });*/
+        }
+      });
     }
-  });*/
-
-  //let color = element.target.value;
-  /*chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {file: 'inject.js'});
-  });*/
-//};
-
-/*changeColor.addEventListener('click', function() {
-  console.log("Hello");
-});*/
+  });
+}
